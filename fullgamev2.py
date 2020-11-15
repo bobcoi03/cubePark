3e7afc0a6e5fa4cceba3e2d5d105f232dd2c4e1e
bobcoi03 <elongatedmusk6969@gmail.com>
2020-11-15 22:57:22 +0700

    put moving functions inside class box

--- fullgamev2.py
+++ fullgamev2.py
@@ -31,19 +31,12 @@ class blackBox:
 	font = pygame.font.Font("freesansbold.ttf",13)
 	screen = pygame.display.set_mode(WINSIZE)
 	white = 255,255,255
-	x = 400
-	y = 300
-	xmove = 0
-	ymove = 0
 
-
-	def __init__(self,boxwidth,boxheight,x,y,xmove,ymove):
+	def __init__(self,boxwidth,boxheight,x,y):
 		self.boxwidth = boxwidth
 		self.boxheight = boxheight
 		self.x = x
 		self.y = y
-		self.xmove = xmove
-		self.ymove = ymove
 
 	def draw(self):
 		blackBox = pygame.draw.rect(screen, white, (x,y,self.boxwidth,self.boxheight))
@@ -55,9 +48,6 @@ class blackBox:
 		yCoordText = font.render("Y: " + str(y), True,(green))
 		screen.blit(xCordText, (10,40))
 		screen.blit(yCoordText, (10,80))
-
-def line():
-	line = pygame.draw.rect(screen, white, (0,150,800,3))
 
 class park:
 	thickness = 2
@@ -72,23 +62,18 @@ class park:
 		self.thickness = thickness
 
 	def draw(self):
-		line1 = pygame.draw.line(screen, self.color,(self.boxXCo,self.boxYCo),(self.boxXCo + self.boxWIDTH,self.boxYCo),thickness)
+		line1 = pygame.draw.line(screen, self.color,(boxXCo, boxYCo),( boxXCo + boxWIDTH, boxYCo),thickness)
 		line2 = pygame.draw.line(screen, self.color,(boxXCo + boxWIDTH,boxYCo),(boxXCo + boxWIDTH,boxYCo + boxHEIGHT), thickness)
 		line3 = pygame.draw.line(screen, self.color,(boxXCo + boxWIDTH,boxYCo + boxHEIGHT),(boxXCo,boxYCo + boxHEIGHT), thickness)
 		line4 = pygame.draw.line(screen, self.color,(boxXCo,boxYCo + boxHEIGHT),(boxXCo,boxYCo), thickness)
 
-	def boundaries(self):
-		pass
-		
 def collision():
 	xCo = 400
 	yCo = 30
 	width = 30
 	height = 30
 	thickness = 2
-
 	#distance = math.sqrt((math.pow(enemyX - bulletX,2)) + (math.pow(enemyY - bulletY,2)))
-
 	i = 0
 	for i in range(0,800):
 		distance = math.sqrt((math.pow(i - x,2)) + (math.pow(150 - y,2)))
@@ -96,46 +81,60 @@ def collision():
 			return True
 	return False
 
+def line():
+	line = pygame.draw.rect(screen, white, (0,150,800,3))
+# rate of movement
+sensitivity = 2
+def left():
+	global x,xmove
+	xmove = - sensitivity
+	x -= xmove
+def right():
+	global x,xmove
+	xmove = + sensitivity
+	x += xmove 
+def forward():
+	global y,ymove
+	ymove = - sensitivity
+	y -= ymove
+def backward():
+	global y,ymove
+	ymove = + sensitivity
+	y += ymove
+
 running = True
 score = 0
 time = 0
 timeInPark = 0
 
-
 while running:
-	sensitivity = 2
 	green = 0,255,0
 	clock = pygame.time.Clock()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
+			pygame.quit()
 			running = False
+			sys.exit()
 		if event.type == pygame.RESIZABLE:
 			screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
-				xmove = 0 - sensitivity
-				x -= xmove
+				left()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
-				xmove = + sensitivity
-				x -= xmove
+				right()
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				xmove = 0
+				xmove = 0			
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
-				ymove = - sensitivity
-				y -= ymove
+				forward()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_DOWN:
-				ymove = + sensitivity
-				y -= ymove
+				backward()
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
 				ymove = 0
-	# score function
-	#distance = math.sqrt((math.pow(enemyX - bulletX,2)) + (math.pow(enemyY - bulletY,2)))
-	#if distance < 0:
 	x += xmove
 	y += ymove
 	if x < 0:
@@ -152,15 +151,16 @@ while running:
 	parky = boxYCo + boxHEIGHT/2
 	middleX = x + 12.5
 	middleY = y + 12.5
-
+	# distance between center of box and park and renders it on screen
 	distance = math.sqrt((math.pow(parkx - middleX,2)) + (math.pow(parky - middleY,2)))
 	distanceText = font.render("DISTANCE: " + str(distance), True,(green))
 	timeText = font.render("Time: " + str(time), True,(green))
-	
+	# When box is inside Park screen prints True
 	insideParkText = font.render("TRUE",1,(green))
 	scoreText = font.render("Score: " + str(score),1,(green))
-
-	box = blackBox(25,25,x,y,xmove,ymove)
+	# defines width,height,(x,y),xmove,ymove of box
+	# Scoring mechanics
+	box = blackBox(25,25,x,y)
 	if distance < 5:
 		timeInPark += 1
 		screen.blit(insideParkText, (400,300))
@@ -168,7 +168,6 @@ while running:
 			timeInPark = 0
 			score += 100
 			box.draw()
-
 
 	screen.blit(distanceText, (10,500))
 	screen.blit(scoreText, (10,400))
---
+++ fullgamev3.py
@@ -0,0 +1,188 @@
+import sys, pygame, time, random, math
+
+pygame.init()
+
+black = 0,0,0
+red = 255,0,0
+green = 0,255,0
+white = 255,255,255
+yellow = 255,255,0
+WINSIZE = WIN_WIDTH, WIN_HEIGHT = 800, 600
+
+screen = pygame.display.set_mode((WINSIZE), pygame.RESIZABLE)
+
+font = pygame.font.Font("freesansbold.ttf",13)
+
+x = 400
+y = 300
+
+xmove = 0
+ymove = 0
+
+boxXCo = 400
+boxYCo = 30
+boxWIDTH = 30
+boxHEIGHT = 30
+
+thickness = 2
+line = pygame.draw.rect(screen, white, (0,150,800,3))
+sensitivity = 2
+
+class blackBox:
+	font = pygame.font.Font("freesansbold.ttf",13)
+	screen = pygame.display.set_mode(WINSIZE)
+	white = 255,255,255
+
+	def __init__(self,boxwidth,boxheight,x,y):
+		self.boxwidth = boxwidth
+		self.boxheight = boxheight
+		self.x = x
+		self.y = y
+
+	def draw(self):
+		blackBox = pygame.draw.rect(screen, white, (x,y,self.boxwidth,self.boxheight))
+
+	def show_coords(self):
+		coordText = font.render("Co-ordinates:", True,(green))
+		screen.blit(coordText, (10,10))
+		xCordText = font.render("X: " + str(x), True,(green))
+		yCoordText = font.render("Y: " + str(y), True,(green))
+		screen.blit(xCordText, (10,40))
+		screen.blit(yCoordText, (10,80))
+
+	def left(self):
+		global x,y,xmove,ymove
+		xmove = - sensitivity
+		x -= xmove
+	def right(self):
+		global x,y,xmove,ymove
+		xmove = + sensitivity
+		x += xmove 
+	def forward(self):
+		global x,y,xmove,ymove
+		ymove = - sensitivity
+		y -= ymove
+	def backward(self):
+		global x,y,xmove,ymove
+		ymove = + sensitivity
+		y += ymove
+
+class park:
+	thickness = 2
+
+	def __init__(self,screen,color,boxXCo,boxYCo,boxWIDTH,boxHEIGHT):
+		self.screen = screen
+		self.boxXCo = boxXCo
+		self.boxYCo = boxYCo
+		self.boxWIDTH = boxWIDTH
+		self.boxHEIGHT = boxHEIGHT
+		self.color = color
+		self.thickness = thickness
+
+	def draw(self):
+		line1 = pygame.draw.line(screen, self.color,(boxXCo, boxYCo),( boxXCo + boxWIDTH, boxYCo),thickness)
+		line2 = pygame.draw.line(screen, self.color,(boxXCo + boxWIDTH,boxYCo),(boxXCo + boxWIDTH,boxYCo + boxHEIGHT), thickness)
+		line3 = pygame.draw.line(screen, self.color,(boxXCo + boxWIDTH,boxYCo + boxHEIGHT),(boxXCo,boxYCo + boxHEIGHT), thickness)
+		line4 = pygame.draw.line(screen, self.color,(boxXCo,boxYCo + boxHEIGHT),(boxXCo,boxYCo), thickness)
+
+def collision():
+	xCo = 400
+	yCo = 30
+	width = 30
+	height = 30
+	thickness = 2
+	#distance = math.sqrt((math.pow(enemyX - bulletX,2)) + (math.pow(enemyY - bulletY,2)))
+	i = 0
+	for i in range(0,800):
+		distance = math.sqrt((math.pow(i - x,2)) + (math.pow(150 - y,2)))
+		if distance < 0.5 or distance < -0.5:
+			return True
+	return False
+
+def line():
+	line = pygame.draw.rect(screen, white, (0,150,800,3))
+# rate of movement
+
+running = True
+score = 0
+time = 0
+timeInPark = 0
+
+while running:
+	green = 0,255,0
+	clock = pygame.time.Clock()
+	
+	screen.fill(black)
+	# defines middle of park and middle of box
+	parkx = boxXCo + boxWIDTH/2
+	parky = boxYCo + boxHEIGHT/2
+	middleX = x + 12.5
+	middleY = y + 12.5
+	# distance between center of box and park and renders it on screen
+	distance = math.sqrt((math.pow(parkx - middleX,2)) + (math.pow(parky - middleY,2)))
+	distanceText = font.render("DISTANCE: " + str(distance), True,(green))
+	timeText = font.render("Time: " + str(time), True,(green))
+	# When box is inside Park screen prints True
+	insideParkText = font.render("TRUE",1,(green))
+	scoreText = font.render("Score: " + str(score),1,(green))
+	# defines width,height,(x,y),xmove,ymove of box
+	# Scoring mechanics
+	box = blackBox(25,25,x,y)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			running = False
+			sys.exit()
+		if event.type == pygame.RESIZABLE:
+			screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				box.left()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				box.right()
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				xmove = 0			
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				box.forward()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_DOWN:
+				box.backward()
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+				ymove = 0
+	x += xmove
+	y += ymove
+	if x < 0:
+		x = 0
+	if x > 775:
+		x = 775
+	if y < 0:
+		y = 0
+	if y > 575:
+		y = 575
+
+	if distance < 5:
+		timeInPark += 1
+		screen.blit(insideParkText, (400,300))
+		if timeInPark == 301:
+			timeInPark = 0
+			score += 100
+			box.draw()
+
+	screen.blit(distanceText, (10,500))
+	screen.blit(scoreText, (10,400))
+	screen.blit(timeText,(10,300))
+	line()
+	
+	box.draw()
+	box.show_coords()
+	
+	parks = park(screen,yellow,boxXCo,boxYCo,boxWIDTH,boxHEIGHT)
+	parks.draw()
+	time += 1
+	clock.tick(60)
+	pygame.display.update()
